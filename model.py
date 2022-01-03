03874acc4a58530f0c9986292b9ef35a74b54ddf
peteris racinskis <peterisracinskis@gmail.com>
2022-01-03 17:44:41 +0200

    DIA_PD properly normalized

--- model.py
+++ model.py
@@ -19,7 +19,7 @@ def coincidence_total(tensor: np.ndarray) -> np.ndarray:
 # received some votes that didn't hit the scoring threshold.
 # Also drop countries that have participated in very few events,
 # they drown out the results from others.
-def edge_average(coincidence: np.ndarray, totals: np.ndarray, index: dict, thresh=0, alpha=0) -> np.ndarray:
+def edge_average(coincidence: np.ndarray, totals: np.ndarray, countries: dict, thresh=0, alpha=0):
     n,_ = coincidence.shape
     alpha_diag = np.identity(coincidence.shape[0]) * alpha
     res = totals / (coincidence + eps)
@@ -29,8 +29,21 @@ def edge_average(coincidence: np.ndarray, totals: np.ndarray, index: dict, thres
             if np.max(coincidence[i,:]) < thresh:
                 res[:,i] = 0
                 res[i,:] = 1
-                dropped.append(index[i])
+                dropped.append(countries[i])
     return res + alpha - alpha_diag, dropped
+
+
+def edge_average_nocor(coincidence: np.ndarray, totals: np.ndarray, countries: dict, thresh=0):
+    n,_ = coincidence.shape
+    res = totals / (coincidence + eps)
+    dropped = []
+    if thresh > 0:
+        for i in range(n):
+            if np.max(coincidence[i,:]) < thresh:
+                res[:,i] = 0
+                res[i,:] = 1
+                dropped.append(countries[i])
+    return res, dropped
 
 def recv_average(edge: np.ndarray) -> np.ndarray:
     return np.average(edge, axis=1)
@@ -60,19 +73,6 @@ def value_by_countries(c1, c2, index: dict, mat: np.ndarray):
     j = index[c2]
     return mat[i][j]
 
-def get_correction_matrix(dataset: np.ndarray, index: dict, countries: dict, norm=False) -> np.ndarray:
-    coincidence = coincidence_count(dataset)
-    totals = coincidence_total(dataset)
-    averages, dropped = edge_average(coincidence, totals, countries, thresh=10, alpha=1)
-    cleared, index, countries = clear_matrix(averages, index, countries, dropped)
-    average_recv = recv_average(cleared)
-    bias_coefficients = bias_matrix(cleared, average_recv)
-    blist = sorted_bias(bias_coefficients, countries)
-    correction_coefficients = corr_matrix(bias_coefficients)
-    dist_matrix = distance_measure(correction_coefficients)
-    d_list = sorted_bias(dist_matrix, countries, inv=True)
-    return corr_matrix(bias_coefficients), dist_matrix, dropped
-
 def correct_scores(dataset: np.ndarray, corr: np.ndarray) -> np.ndarray:
     negative_comp = np.where(dataset == -1, -1, 0)
     nonnegat_comp = np.where(dataset == -1, 0, dataset)
@@ -95,9 +95,36 @@ def clear_matrix(dataset: np.ndarray, index: dict, countries: dict, dropped: lis
 def distance_measure(corr: np.ndarray) -> np.ndarray:
     return (corr + corr.T) / 2
 
-def normalized_total(total: np.ndarray, alpha=1) -> np.ndarray:
-    N = np.sum(total + alpha - np.identity(total.shape[0]))
+# STEPS:
+# 1. get probability a vote is j -> i
+# 2. find quality correction: p_i * c_i = 1/n forall i
+# 3. find bias correction: p_ij * c_i * c_j = 1/nn for all ij
+# 4. bias correction can be applied to scores when converted to prob
+def normalized_score(total: np.ndarray, alpha=1) -> np.ndarray:
+    total = total + alpha - np.identity(total.shape[0]) * alpha
+    N = np.sum(total)
     return total / N
+
+# 2d probability distribution, get corrections to produce uniform distribution
+def corrections(matrix: np.ndarray) -> np.ndarray:
+    N, _ = matrix.shape
+    row_probabilities = np.sum(matrix, axis=1)
+    row_corrections = 1 / (N * row_probabilities + eps)
+    cell_corrections = (1 / (N ** 2 * matrix.T * row_probabilities + eps)).T
+    cell_corrections = np.where(np.identity(N) == 1, 0, cell_corrections)
+    return row_corrections, cell_corrections
+
+def get_correction_matrix(dataset: np.ndarray, index: dict, countries: dict, norm=False) -> np.ndarray:
+    coincidence = coincidence_count(dataset)
+    totals = coincidence_total(dataset) # raw totals - more coincident, more score! need to average
+    avg, dropped = edge_average_nocor(coincidence, totals, countries, thresh=10) # mark low incidence countries for removal, add small base score constant
+    cleared_avg, index, countries = clear_matrix(avg, index, countries, dropped) # remove low incidence countries
+    score_probability = normalized_score(cleared_avg, alpha=0.5)
+    quals, cors = corrections(score_probability)
+    correction_matrix = cors.T
+    return quals, correction_matrix
+
+
 
 if __name__ == "__main__":
     shapefile = INFILE + "-shape.txt"
@@ -112,7 +139,9 @@ if __name__ == "__main__":
     with open(countryfile, 'r') as f:
         countries = {int(z):y[:-1] for z,y in [x.split("\t") for x in f.readlines()]}
         index = {v:k for k,v in countries.items()}
+    
     arr = np.reshape(np.fromfile(datafile, dtype=np.int64), shape)
+    '''
     coincidence = coincidence_count(arr)
     totals = coincidence_total(arr)
     averages, dropped = edge_average(coincidence, totals, countries, thresh=10, alpha=0.5)
@@ -123,7 +152,8 @@ if __name__ == "__main__":
     correction_coefficients = corr_matrix(bias_coefficients)
     dist_matrix = distance_measure(correction_coefficients)
     d_list = sorted_bias(dist_matrix, countries, inv=True)
-
+    '''
+    average_quality, correction_matrix = get_correction_matrix(arr, index, countries)
 
 
     biasfile = OUTFILE + "-bias.txt"
